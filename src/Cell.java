f4a7dbaae94bf322eb3a5511ec1192c2a8f2b531
thomasdboer <t.d.boer@student.tue.nl>
2016-10-15 19:53:39 +0200

    First GUI work done

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,6 @@
+//Thomas Willems, Thomas den Boer
+//15-10-2016
+//This class represents a cell within the Game of Life
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -30,16 +33,20 @@ class Cell extends JLabel{
     }
 
     void update(){
-        if(numNeighbours<2){
+        //If an alive cell has less than two neighbours, it dies
+        if(alive && numNeighbours<2){
             setAlive(false);
         }
-        else if(numNeighbours>3){
+        //If an alive cell has more than three neighbours, it dies
+        else if(alive && numNeighbours>3){
             setAlive(false);
         }
-        else if(numNeighbours==2||numNeighbours==3){
+        //If an alive cell has two or three neighbours, it will live on
+        else if(alive && (numNeighbours==2||numNeighbours==3)){
             setAlive(true);
         }
-        else if(!alive&&numNeighbours==3){
+        //If a dead cell has three neighbours, it comes back to life
+        else if(!alive && numNeighbours==3){
             setAlive(true);
         }
     }
--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -13,25 +13,32 @@ public class GameOfLife{
     int row;
     int col;
     Cell[][] grid = new Cell[row][col];
-    String birthFileName = "birth.txt";
+    String birthFileName = "src\\birth.txt";
     File birthFile = new File(birthFileName);
     Scanner sc;
 
-    void calculateNumNeighbours(){
+    void initJFrame(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel(new BorderLayout());
+        frame.add(panel);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+    }
+    void calculateNumNeighbours() {
         //Iterate over grid
-        for(int i=0; i < grid.length; i++){
-            for(int j=0; j< grid[i].length;j++){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 //Create an integer to save amount of found neighbouring cells
                 int neighbours = 0;
-                //This int resets to 0 for every iteration
+                //This int resets to 0 for every grid position
                 //Iterate over positions around chosen grid position
-                for(int k=(i-1); k<=(i+1); k++){
-                    for(int l=(j-1); l<=(j+1);l++){
-                        if(grid[k][l].isAlive()){
+                for (int k = (i - 1); k <= (i + 1); k++) {
+                    for (int l = (j - 1); l <= (j + 1); l++) {
+                        if (grid[k][l].isAlive()) {
                             neighbours++;
                         }
                         //Make sure chosen grid position is not included
-                        if(k==i&&j==l){
+                        if (k == i && j == l) {
                             neighbours--;
                         }
                     }
@@ -41,41 +48,50 @@ public class GameOfLife{
             }
         }
     }
-    void readInitial(){
+
+    void readInitial() {
         //Open birthFile and handle exceptions
-        try{
+        try {
             sc = new Scanner(birthFile);
-        }catch(FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
+            System.out.println("Did not find file!");
         }
-        //Read values for amount of columns and rows
-        row = sc.nextInt();
-        col = sc.nextInt();
+        //Read values for amount of columns and rows and throw exceptions if not given
+        try{row=sc.nextInt();}catch(NullPointerException e){System.out.println("No row given in file!");};
+        try{col=sc.nextInt();}catch(NullPointerException e){System.out.println("No column given in file!");};
+        //Initialize the grid to the size given
+        Cell[][] grid = new Cell[row][col];
         //Create a Cell object at every grid position
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i < (row-1); i++) {
+            for (int j = 0; j < (col-1); j++) {
                 grid[i][j] = new Cell();
             }
         }
         //Read grid and set cell states
-        while(sc.hasNext()){
-            for(int i = 0; i < col; i++){
-                for(int j = 0; j < row; j++){
-                    if(sc.next()=="*"){
-                        grid[i][j].setAlive(true);
-                    }
-                    else if(sc.next()=="."){
-                        grid[i][j].setAlive(false);
-                    }
+        for (int i = 0; i < (row-1); i++) {
+            for (int j = 0; j < (col-1); j++) {
+                String next = sc.next();
+                if (next.equals("*")) {
+                    grid[i][j].setAlive(true);
+                } else if (next.equals(".")) {
+                    grid[i][j].setAlive(false);
                 }
             }
         }
     }
-    void nextGeneration(){
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+
+    void nextGeneration() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 grid[i][j].update();
             }
         }
     }
-
+    public static void main(String[] args) {
+        new GameOfLife().initJFrame();
+        new GameOfLife().readInitial();
+        new GameOfLife().calculateNumNeighbours();
+        new GameOfLife().nextGeneration();
+    }
 }
+
